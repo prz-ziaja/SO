4e503c80e2ba184acdfbfcecb6b6af98ee72d476
Maryna05 <luka4ik.marina@gmail.com>
2020-03-26 14:51:36 +0100

    c

--- MLukachyk/lab2/SO_lab2/cmake-build-debug/ex2.c
+++
@@ -1,8 +0,0 @@
-//
-// Created by Марина Лукачик on 2020-03-24.
-//
-
-//Operacje pisania i czytania z pliku
-/*[writeall]*/
-
-
--- MLukachyk/lab2/SO_lab2/t.h
+++
@@ -1,9 +0,0 @@
-//
-// Created by Марина Лукачик on 2020-03-25.
-//
-
-#ifndef SO_LAB2_T_H
-#define SO_LAB2_T_H
-void timestart(void);
-void timestop(char *msg);
-#endif //SO_LAB2_T_H
