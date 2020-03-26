421eafa0e0e719cb1891d25a87db1ad32d382df9
adamklekowski <klekowskiadam@gmail.com>
2020-03-26 11:27:11 +0100

    lab 2

---
+++ AKlekowski/lab2/readall.c
@@ -0,0 +1,35 @@
+/*[writeall]*/
+ssize_t writeall(int fd, const void *buf, size_t nbyte)
+{
+	ssize_t nwritten = 0, n;
+
+	do {
+		if ((n = write(fd, &((const char *)buf)[nwritten], nbyte - nwritten)) == -1) {
+			if (errno == EINTR)
+				continue;
+			else
+				return -1;
+		}
+		nwritten += n;
+	} while (nwritten < nbyte);
+	return nwritten;
+}
+
+/*[readall]*/
+ssize_t readall(int fd, void *buf, size_t nbyte)
+{
+ ssize_t nread = 0, n;
+ do {
+	 if ((n = read(fd, &((char *)buf)[nread], nbyte - nread)) == -1) {
+		 if (errno == EINTR)
+				continue;
+		 else
+				exit(EXIT_FAILURE);
+	 }
+	 if (n == 0)
+		 return nread;
+		 nread += n;
+	 } while (nread < nbyte);
+ 
+ return nread;
+}
