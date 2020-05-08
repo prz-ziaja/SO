aa1cecd0e5f344b2abe9d676a212f4770b3bb9ca
Piotr Krzesniak <peterczak@wp.pl>
2020-05-08 14:20:05 +0200

    add lab8

---
+++ PKrzesniak/LAB8/SHM/unix.h
@@ -0,0 +1,13 @@
+#include <sys/ipc.h>
+#include <sys/sem.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
+#include <string.h>
+#include <time.h>
+
+//#define max(a,b) ((a)>(b)?(a):(b))
