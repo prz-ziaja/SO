e750e7063719048f07486a0c5b4d4905983e89e2
Przemek <prz.ziaja@gmail.com>
2020-05-25 23:35:52 +0200

    lab12 PZ

---
+++ PZiaja/lab12/gethostbyname-demo.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <errno.h>
+#include <netdb.h>      /* 4 gethostbyname, hostent structure */
+#include <unistd.h>     /* 4 exit */
+#include <netinet/in.h> /* 4 ntohn */
+
+int main(int argc, char *argv[])
+{
+     int i,j;
+     struct hostent *he;
+
+     if (argc != 2) 
+     {
+          fprintf(stderr,"usage: %s hostname\n", argv[0]);
+          return 1;
+     }
+
+     if ((he = gethostbyname(argv[1])) == NULL) 
+     {  
+        if (h_errno == NO_RECOVERY)
+            printf("Server can't return address\n.");
+        else if (h_errno == TRY_AGAIN)
+            printf("Server has problem, try later.\n");
+        else if(h_errno == HOST_NOT_FOUND)
+            printf("Didn't find host.\n");
+        else if (h_errno == NO_DATA)
+            printf("Name ok, no ip addr.\n");
+
+          return 1;
+     }
+
+     /* host info: */
+     printf("\nHost name: %s", he->h_name);
+     printf("\nAliases:");
+     for(i=0;he->h_aliases[i] != NULL;++i)
+          printf("\n%d. %s", i+1, he->h_aliases[i]);
+     if(he->h_addrtype == AF_INET)
+          printf("\nAddres type: IPv4");
+     if(he->h_addrtype == AF_INET6)
+          printf("\nAddres type: IPv6");
+     printf("\nAddress length: %d bytes", he->h_length);
+     printf("\nAddresses:");
+     for(i=0;he->h_addr_list[i]!=0;i++){
+	     for(j=0;j<he->h_length;++j)
+	     {
+		  printf("%d", (uint8_t)he->h_addr_list[i][j]);
+		  if(j < (he->h_length-1))
+		       printf(".");
+	     }
+     printf("\n");}
+
+     return 0;
+}
---
+++ PZiaja/lab12/hostinfo.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+
+void display_hostent(struct hostent *h){
+	int i;
+	
+	printf("name: %s; type: %d; len: %d\n", h->h_name, h->h_addrtype, h->h_length);
+	for (i=0; h->h_aliases[i] != NULL; i++)
+		printf("\t%s\n", h->h_aliases[i]);
+	if (h->h_addrtype == AF_INET) {
+		for (i=0; h->h_addr_list[i] != NULL; i++)
+			printf("\t%s\n", inet_ntoa(*((struct in_addr *)h->h_addr_list[i])));
+	} 
+}
+
+void hostdb(){
+	struct hostent *h;
+	
+	sethostent(1);
+	while ((h = gethostent())!= NULL)
+		display_hostent(h);
+	endhostent();
+}
+
+int main(int argc, char *argv[]){
+	hostdb();
+	return 0;
+}
